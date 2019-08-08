ef00cfe7183df3ffd73781f05b16815241bdf12b
kangkimsim <kangkimsim@gmail.com>
2019-08-08 16:22:20 +0800

    Hide postgres connection

--- db_connection.js
+++ db_connection.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const cn = 'postgres://postgres:qawsed@localhost:5432/ProjectResourceMngmt' // change to your db connection
+const cn = 'postgres://username:password@host:port/database' // change to your db connection
 const db = pgp(cn);
 
 module.exports = db
